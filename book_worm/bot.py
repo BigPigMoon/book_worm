5ef98c84d9e5971b58c9379ce4185bccd4a701bd
bigpigmoon <matvey.novikov02@gmail.com>
2023-04-24 09:24:12 +0700

    feat(user): add user registration in data base

--- book_worm/bot.py
+++ book_worm/bot.py
@@ -1,10 +1,15 @@
+"""Main bot module"""
+
 import os
-from dotenv import load_dotenv
 import logging
+
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 import messages
+import services
+from models import db, user
 
 logging.basicConfig(
     format='%(asctime)s:%(name)s:%(levelname)s:%(message)s',
@@ -12,23 +17,51 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger()
+
 load_dotenv()
 
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_chat is None in /start")
+        return
+
+    services.user.registration(update.message.from_user['id'])
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
+        chat_id=effective_chat.id,
         text=messages.GREETING,
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_chat is None in /help")
+        return
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
+        chat_id=effective_chat.id,
         text=messages.HELP,
     )
 
+
+async def category_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_chat is None in /categories")
+        return
+
+    await context.bot.send_message(
+        chat_id=effective_chat.id,
+        text="",
+    )
+
 if __name__ == '__main__':
+    with db.db:
+        db.db.create_tables([user.User,])
+
     application = ApplicationBuilder().token(
         os.getenv('TELEGRAM_BOT_TOKEN')
     ).build()
---
+++ book_worm/services/user.py
@@ -0,0 +1,22 @@
+import logging
+
+from models.user import User
+
+
+logger = logging.getLogger()
+
+
+def registration(uid: int):
+    """Registrate new user."""
+    if not uid:
+        logger.warning("User id does not exist")
+        return
+
+    user: User
+    try:
+        user = User.select().where(User.telegram_id == uid).get()
+    except User.DoesNotExist:
+        user = None
+
+    if not user:
+        User.create(telegram_id=uid)
